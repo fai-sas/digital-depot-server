e79efc314b2a07fc4a9cea5583221b0b170aa030
fai-sas <craftedbyfaisal@gmail.com>
2024-09-28 20:47:48 +0600

    user routes added

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,5 +1,60 @@
 import express from 'express'
+import validateRequest, {
+  validateRequestCookies,
+} from '../../middlewares/validateRequest'
+import { UserControllers } from './user.controller'
+import { USER_ROLE } from './user.constant'
+import auth from '../../middlewares/auth'
+import { parseBody } from '../../middlewares/bodyParser'
+import { upload } from '../../utils/sendImageToCloudinary'
 
 const router = express.Router()
+
+router.get('/', auth(USER_ROLE.ADMIN), UserControllers.getAllUsers)
+
+router.get(
+  '/profile',
+  auth(USER_ROLE.ADMIN, USER_ROLE.USER),
+  UserControllers.getCurrentUserProfile
+)
+
+router.get(
+  '/:id',
+  auth(USER_ROLE.ADMIN, USER_ROLE.USER),
+  UserControllers.getSingleUser
+)
+
+router.put(
+  '/make-admin/:id',
+  auth(USER_ROLE.ADMIN),
+  UserControllers.makeUserAnAdmin
+)
+
+router.put('/block-user/:id', auth(USER_ROLE.ADMIN), UserControllers.blockUser)
+
+router.put(
+  '/delete-user/:id',
+  auth(USER_ROLE.ADMIN),
+  UserControllers.deleteUser
+)
+
+// router.patch(
+//   '/profile',
+//   auth(USER_ROLE.ADMIN, USER_ROLE.USER),
+//   multerUpload.single('profilePhoto'),
+//   parseBody,
+//   UserControllers.updateCurrentUserProfile
+// )
+
+// router.patch(
+//   '/profile',
+//   auth(USER_ROLE.ADMIN, USER_ROLE.USER),
+//   upload.single('profilePhoto'),
+//   (req: Request, res: Response, next: NextFunction) => {
+//     req.body = JSON.parse(req.body.data)
+//     next()
+//   },
+//   UserControllers.updateCurrentUserProfile
+// )
 
 export const UserRoutes = router
