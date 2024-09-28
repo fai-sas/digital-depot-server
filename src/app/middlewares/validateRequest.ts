fc5b204c93eff983a4e6ee54a1c27f9183700cef
fai-sas <craftedbyfaisal@gmail.com>
2024-09-28 19:17:59 +0600

    middlewares added

---
+++ src/app/middlewares/validateRequest.ts
@@ -0,0 +1,45 @@
+// import { NextFunction, Request, Response } from 'express'
+// import { AnyZodObject } from 'zod'
+// import catchAsync from '../utils/catchAsync'
+
+// const validateRequest = (schema: AnyZodObject) => {
+//   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+//     await schema.parseAsync({
+//       body: req.body,
+//       cookies: req.cookies,
+//     })
+//     next()
+//   })
+// }
+
+// export default validateRequest
+
+import { NextFunction, Request, Response } from 'express'
+import { AnyZodObject } from 'zod'
+import catchAsync from '../utils/catchAsync'
+
+const validateRequest = (schema: AnyZodObject) => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const parsedBody = await schema.parseAsync({
+      body: req.body,
+    })
+
+    req.body = parsedBody.body
+
+    next()
+  })
+}
+
+export const validateRequestCookies = (schema: AnyZodObject) => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const parsedCookies = await schema.parseAsync({
+      cookies: req.cookies,
+    })
+
+    req.cookies = parsedCookies.cookies
+
+    next()
+  })
+}
+
+export default validateRequest
